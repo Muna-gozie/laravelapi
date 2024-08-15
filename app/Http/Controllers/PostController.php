6c3561b85eb73d1b82abc6c672ec9426d34906d9
Muna-gozie <muna.chukwuma@gmail.com>
2024-08-15 01:12:21 +0100

    push updates to repo

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,12 +5,22 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 //use App\Http\Requests\StorePostRequest;
 use App\Http\Requests\UpdatePostRequest;
+use Illuminate\Auth\Access\Gate;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controllers\HasMiddleware;
+use Illuminate\Routing\Controllers\Middleware;
 
 //use http\Client\Request;
 
-class PostController extends Controller
+class PostController extends Controller implements HasMiddleware
 {
+    // this function ensures that auth:sanctum authentication middleware i.e (to protect the api route against users without api token) and  is applied on all methods of this class except (index and show) methods. HasMiddleware interface must be implemented for this to work.
+
+    public static function middleware(){
+        return [
+            new Middleware('auth:sanctum', except: ['index','show'])
+        ];
+    }
     /**
      * Display a listing of the resource.
      */
@@ -31,9 +41,9 @@ class PostController extends Controller
             'body' => 'required|max:255'
         ]);
 
-//        $result= Post::create($fields);
-//        return $result;
+        $post = $request->user()->post()->create($fields);
 
+        return $post;
     }
 
     /**
@@ -50,6 +60,8 @@ class PostController extends Controller
      */
     public function update(Request $request, Post $post)
     {
+        // Gate: authorize parameters includes, method name, user, and post but the user is gotten automatically by the Gate class
+        Gate::authorize('modify', $post);
         //
         $fields = $request->validate([
             'title' => ['required','unique:posts','max:255'],
@@ -66,6 +78,7 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
+        Gate::authorize('modify', $post);
         $post->delete();
 
         return ['message' => 'Post deleted successfully'];
